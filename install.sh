2c722053f8a0392a4e1f752395d7e32855f3f03c
Artyom Goncharov <m4yers@gmail.com>
2016-11-05 15:28:17 +0200

    Add vim plugins autoinstall

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ export here="$( cd "$( dirname "$0" )" && pwd )"
 for install in $(ls $here/*/install.sh) ; do
     source $install
 done
+
+# Vim
+# install everything only it is a fresh install
+vundle=~/.vim/bundle/Vundle.vim
+if [ ! -f $vundle ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $vundle
+    vim +PluginInstall +qall
+fi
