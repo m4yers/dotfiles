26f401ba8728fc1d909126508918cacfe7ca543a
Artyom Goncharov <m4yers@gmail.com>
2017-09-13 22:19:38 +0300

    Add all, system, home and help subcommands

--- install.sh
+++ install.sh
@@ -4,19 +4,27 @@ export ROOT="$( cd "$( dirname "$0" )" && pwd )"
 
 source $ROOT/scripts/shared.sh
 
-if ! is_mac; then
-  echo "Mac only installer is available"
-  exit 1
-fi
+check_system() {
+  if ! is_mac; then
+    echo "Mac only installer is available"
+    exit 1
+  fi
+}
 
-# source $ROOT/scripts/macos.sh
+setup_system() {
+  source $ROOT/scripts/macos.sh
+}
 
-source $ROOT/scripts/bash.sh
-source $ROOT/scripts/brew.sh
-source $ROOT/scripts/pip.sh
-source $ROOT/scripts/npm.sh
+setup_home() {
+  source $ROOT/scripts/bash.sh
+  source $ROOT/scripts/brew.sh
+  source $ROOT/scripts/pip.sh
+  source $ROOT/scripts/npm.sh
 
-install() {
+  bash_init
+  brew_init
+  pip_init
+
   for installer in $(ls $ROOT/*/install.sh); do
     install_brew_requirements $installer
     install_cask_requirements $installer
@@ -24,21 +32,72 @@ install() {
     install_npm_requirements $installer
 
     # If this setup to be moved to other systems each installer must provide
-    # system specific install routine
+    # setup_system specific setup_home routine
     source $installer
     install
   done
-}
-
-main() {
-  bash_init
-  brew_init
-  pip_init
-
-  install
 
   pip_fini
   brew_fini
 }
 
-main
+print_help() {
+  echo "Usage:"
+  echo "    all         Run all features"
+  echo "    system      Run system setup"
+  echo "    home        Run home directory setup"
+  echo "    help        Print help"
+}
+
+main() {
+  check_system
+
+  local option_system=false
+  local option_install=false
+
+  if [[ $# -eq 0 ]]; then
+    print_help
+    exit 1
+  fi
+
+  while [[ $# -ne 0 ]]
+  do
+    option="$1"
+    shift
+
+    case $option in
+      all)
+        option_system=true
+        option_install=true
+        ;;
+      system)
+        option_system=true
+        ;;
+      home)
+        option_install=true
+        ;;
+      help)
+        print_help
+        exit 0
+        ;;
+      *)
+        print_help
+        exit 1
+        ;;
+    esac
+  done
+
+  if ! yesno "Do you really want this?" "no"; then
+    exit 0
+  fi
+
+  if $option_system; then
+    setup_system
+  fi
+
+  if $option_install; then
+    setup_home
+  fi
+}
+
+main "$@"
--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 is_mac() {
   unamestr=`uname`
   test $unamestr == 'Darwin'
@@ -8,4 +9,29 @@ is_mac() {
 is_linux() {
   unamestr=`uname`
   test $unamestr == 'Linux'
+}
+
+yesno() {
+  local display=$1; shift
+  local default=${1,,}; shift
+  while true
+  do
+    echo "${display} Yes/No? (Default: $default)"
+    read -r answer
+    [[ -z "$answer" ]] && answer=$default
+
+    answer=${answer,,}
+    case $answer in
+      yes)
+        return 0
+        ;;
+      no)
+        return 1
+        ;;
+      *)
+        echo "You must choose between \"yes\" or \"no\""
+        exit 1
+        ;;
+    esac
+  done
 }
