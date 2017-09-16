ef8dc4f8b63419231c7aec65f10c8117afaa344a
Artyom Goncharov <m4yers@gmail.com>
2017-09-17 00:43:30 +0300

    [bash] Add man colors

--- bash/bashrc.config.sh
+++ bash/bashrc.config.sh
@@ -16,7 +16,15 @@ export LC_ALL='en_US.UTF-8';
 # Add colors to less
 export LESSOPEN="| pygmentize -f terminal -O style=native -g %s"
 export LESS='-R'
-export MANPAGER="less -+X -is"
+
+# Less Colors for Man Pages
+export LESS_TERMCAP_mb=$'\e[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\e[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\e[0m'           # end mode
+export LESS_TERMCAP_se=$'\e[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\e[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\e[0m'           # end underline
+export LESS_TERMCAP_us=$'\e[04;38;5;146m' # begin underline]]]
 
 # Increase Bash history size. Allow 32³ entries; the default is 500.
 export HISTSIZE='32768';
