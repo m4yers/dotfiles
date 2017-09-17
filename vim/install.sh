e39032fe57219c6b7edc8d2ed10aa8c7b277b5a0
Artyom Goncharov <m4yers@gmail.com>
2017-09-17 12:03:34 +0300

    [vim] sync

--- vim/install.sh
+++ vim/install.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 #brew: curl, ag
-#brew: macvim --with-override-system-vim
+#cask: macvim --with-override-system-vim
 #pip:  pylint, bashate
 #npm:  jsonlint, eslint
 install() {
   ln -s -f $ROOT/vim/vimrc ~/.vimrc
   ln -s -f $ROOT/vim/cvimrc ~/.cvimrc
   ln -s -f $ROOT/vim/vimrc ~/.ideavimrc
+  ln -s -f $ROOT/vim/ycm.py ~/.ycm.py
 
   if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     vim +PlugInstall +qall
   fi
+
+  echo >> $BASHRC
+  echo "# Vim" >> $BASHRC
+  echo "source $ROOT/vim/bashrc.aliases.sh" >> $BASHRC
 }
