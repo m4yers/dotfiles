6ba8a05f030724a224f2097e5854a532310eac98
Artyom Goncharov <artyom.goncharov@guardsquare.com>
2017-12-14 08:47:19 +0100

    Update git configs

--- installers/git/install.sh
+++ installers/git/install.sh
@@ -1,13 +1,13 @@
 #brew: git, git-lfs
 install() {
-  local DEST=~/.config/git
-
-  if [[ ! -a $DEST ]]; then
-    mkdir $DEST
+  local global_gitignore=$HOME/.gitconfig
+  if [[ ! -f $global_gitignore ]]; then
+    echo "${global_gitignore} does not exist. Creating..."
+    touch $global_gitignore
   fi
 
-  ln -s -f $THIS/gitconfig $DEST/.gitconfig
-  ln -s -f $THIS/gitignore $DEST/.gitignore
+  ln -s -f $THIS/gitconfig $HOME/.config/git/config
+  ln -s -f $THIS/gitignore $HOME/.gitignore
 
   echo >> $BASHRC
   echo "# Git" >> $BASHRC
